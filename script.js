717a997fe319a985352d031bc18731cf80dd19a8
Dany123-haha <60242342+Dany123-haha@users.noreply.github.com>
2020-01-23 14:43:02 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$(".math").click(function() {
+    $(".Math").append("<br>functions! ");
+});
+
+$(".American Literature").click(function() {
+    $(".Literature").append("<br>HTML! CSS! jQuery! ");
+});
+
+$("img.history").click(function() {
+    $(".history").append("<br>Native Americans! ");
+});
+
+$("img.english").click(function() {
+    $(".english").append("<br>how to write an essay! ");
+});
